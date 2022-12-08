f278a2e0e6e08543cb0f8415604211f6a401a9dd
Vladimir Kamyshanov <viator3m@gmail.com>
2022-12-08 17:08:12 +0300

    Add logger, update dependencies

---
+++ logger.py
@@ -0,0 +1,10 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(funcName)s - [%(levelname)s] - %(message)s'
+)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
